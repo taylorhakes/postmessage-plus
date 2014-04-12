278ddc4d10a5101aaf403ec3a9ffb7aa42ab6430
Taylor Hakes <taylor@taylorhakes.com>
2014-04-12 16:30:21 -0400

    Added longer id to avoid collisions

--- PMPlus.js
+++ PMPlus.js
@@ -191,7 +191,7 @@
 		var text = "";
 		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-		for (var i = 0; i < 10; i++) {
+		for (var i = 0; i < 20; i++) {
 			text += possible.charAt(Math.floor(Math.random() * possible.length));
 		}
 
